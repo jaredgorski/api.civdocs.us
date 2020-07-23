c1b0384211b64897da72d0132591fd5c191bad21
jaredgorski <jared.gorski@liferay.cloud>
2020-07-22 22:21:28 -0700

    Remove dist from tracked files

--- dist/src/controllers/home.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.index = void 0;
-const package_json_1 = require("../../package.json");
-exports.index = (req, res) => {
-    res.json({ author: package_json_1.author, homepage: package_json_1.homepage, version: package_json_1.version });
-};
-//# sourceMappingURL=home.js.map
